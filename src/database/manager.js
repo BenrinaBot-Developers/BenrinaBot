6831b4351a2a4caeedf03b56b48382c731cdf511
uni-kakurenbo <kohma1116@ybb.ne.jp>
2021-11-27 14:11:29 +0900

    refactor

--- src/database/manager.js
+++ src/database/manager.js
@@ -1,9 +1,9 @@
-'use strict';
+"use strict";
 
-const {workerData} = require('worker_threads')
+const { workerData } = require("worker_threads");
 
 const { Collection } = require("discord.js");
-const { client } = require("../util.js")
+const { client } = require("../util.js");
 
 const { isObject } = require("../functions.js");
 const Logger = require("../system/Logger.js");
@@ -11,24 +11,22 @@ const { DatabaseError } = require("../system/ApplicationError.js");
 
 const fs = require("fs");
 
-const { BaseDatabaseAccessor } = require("./accessor.js");
-
 class Cache {
-  #intervals
-  #lastKeyName
-  #initialData
-  #tags
-  constructor(_type, {level:_level = 1, name:_name, lower:_lowerCache} = {}) {
+  #intervals;
+  #lastKeyName;
+  #initialData;
+  #tags;
+  constructor(_type, { level: _level = 1, name: _name, lower: _lowerCache } = {}) {
     Object.defineProperties(this, {
-      type: {value: _type},
-      level: {value:  _level},
-      lower:  {value:  _lowerCache}
+      type: { value: _type },
+      level: { value: _level },
+      lower: { value: _lowerCache },
     });
-    this.#initialData = `{"data":{}}`
+    this.#initialData = `{"data":{}}`;
     this.#lastKeyName = "lastUpdateAt";
     this.#tags = [];
-  };
-  async begin(){
+  }
+  async begin() {
     this.fileSelecter = await this.#getNewerFileSelecter();
     return this;
   }
@@ -36,14 +34,14 @@ class Cache {
   clear() {
     return Promise.allSettled([
       fs.promises.writeFile(this.#getPath("A"), this.#initialData),
-      fs.promises.writeFile(this.#getPath("B"), this.#initialData)
+      fs.promises.writeFile(this.#getPath("B"), this.#initialData),
     ]);
   }
 
   async get(_keys) {
     const cache = await this.#read();
-    const response = new Collection;
-    _keys.forEach(_key => response.set(_key, cache.data[_key]));
+    const response = new Collection();
+    _keys.forEach((_key) => response.set(_key, cache.data[_key]));
     return response;
   }
   async set(_object = {}, _options) {
@@ -54,18 +52,18 @@ class Cache {
   async delete(_keys, _options) {
     const cache = await this.#read();
     //console.log(cache, _keys);
-    _keys.forEach(_key => {
+    _keys.forEach((_key) => {
       delete cache.data[_key];
     });
     //console.log(cache);
     return this.$write(cache, _options);
-  } 
+  }
 
   #read() {
     return this.#readFile(this.#currentPath);
   }
-  async $write(_data, {writeback = true} = {}) {
-    if(writeback) this.#updateLower(_data);
+  async $write(_data, { writeback = true } = {}) {
+    if (writeback) this.#updateLower(_data);
     /*if(waitWriteback) await this.#updateLower(_data, {waitWriteback:true});
     else */
     this.#tags = Object.keys(_data.data);
@@ -78,9 +76,9 @@ class Cache {
   #updateLower(_cache, _options) {
     return this?.lower?.set(_cache.data, _options);
   }
-  
+
   async #readFile(_path) {
-    const jsonStr = await fs.promises.readFile(_path, 'utf8') || this.#initialData;
+    const jsonStr = (await fs.promises.readFile(_path, "utf8")) || this.#initialData;
     return JSON.parse(jsonStr);
   }
   async #writeFile(_path, _data = this.initialData) {
@@ -95,7 +93,7 @@ class Cache {
   }
   get #currentPath() {
     return this.#getPath(this.fileSelecter ? "A" : "B");
-  } 
+  }
   #getPath(_selecter) {
     return `${__dirname}/cache/L${this.level}/${_selecter}/${this.type}.json`;
   }
@@ -106,26 +104,26 @@ class Cache {
 }
 
 class CacheManager {
-  #begun
-  constructor(_type, _options){
+  #begun;
+  constructor(_type, _options) {
     this.#begun = false;
     Object.defineProperties(this, {
-      L2: { value: new Cache(_type, {level: 2, ..._options}) }
+      L2: { value: new Cache(_type, { level: 2, ..._options }) },
     });
     Object.defineProperties(this, {
-      L1: { value: new Cache(_type, {level: 1, lower: this.L2, ..._options}) }
+      L1: { value: new Cache(_type, { level: 1, lower: this.L2, ..._options }) },
     });
   }
 
   async begin() {
-    await Promise.all([this.L1.begin(), this.L2.begin()])
+    await Promise.all([this.L1.begin(), this.L2.begin()]);
     await this.L1.clear();
     this.#begun = true;
     return this;
   }
   async clear() {
     this.checkBegun();
-    await Promise.all([this.L1.clear(), this.L2.clear()])
+    await Promise.all([this.L1.clear(), this.L2.clear()]);
     return this;
   }
 
@@ -139,43 +137,43 @@ class CacheManager {
     keys = getRestKeys(response);
     //console.log(response, keys)
     response.sweep((_, _key) => keys.includes(_key));
-    Logger.log(Logger.Type.DATABASE, {method: "get", data: {data:Object.fromEntries(response), rest:keys}});
-    return {data:response, rest:keys};
+    Logger.log(Logger.Type.DATABASE, { method: "get", data: { data: Object.fromEntries(response), rest: keys } });
+    return { data: response, rest: keys };
     function getRestKeys(_collection) {
-      return [..._collection.filter((_value, _key) => _value === void 0).keys()]
+      return [..._collection.filter((_value, _key) => _value === void 0).keys()];
     }
   }
 
   async set(_key, _value) {
     this.checkBegun();
-    if(typeof _key !== "string")  throw new DatabaseError("Invalid Key Type: " + _key)
-    const data = {[_key]: _value};
-    await this.L1.set(data)
-    Logger.log(Logger.Type.DATABASE, {method: "set", data: data});
+    if (typeof _key !== "string") throw new DatabaseError("Invalid Key Type: " + _key);
+    const data = { [_key]: _value };
+    await this.L1.set(data);
+    Logger.log(Logger.Type.DATABASE, { method: "set", data: data });
     return this;
   }
   async put(_object = {}) {
     this.checkBegun();
-    if(!isObject(_object)) throw new DatabaseError("Invalid Type: " + _object)
-    await this.L1.set(_object)
-    Logger.log(Logger.Type.DATABASE, {method: "put", data: _object});
+    if (!isObject(_object)) throw new DatabaseError("Invalid Type: " + _object);
+    await this.L1.set(_object);
+    Logger.log(Logger.Type.DATABASE, { method: "put", data: _object });
     return this;
   }
-  
+
   async delete(..._keys) {
     this.checkBegun();
     _keys = _keys.flat(Infinity);
     await Promise.allSettled([
-      this.L1.delete(_keys, {writeback: false}),
-      this.L2.delete(_keys, {writeback: false})
+      this.L1.delete(_keys, { writeback: false }),
+      this.L2.delete(_keys, { writeback: false }),
     ]);
-    Logger.log(Logger.Type.DATABASE, {method: "delete", data: _keys});
+    Logger.log(Logger.Type.DATABASE, { method: "delete", data: _keys });
     return this;
   }
 
   checkBegun() {
-    if(this.#begun) return;
-    throw new DatabaseError("The .begin() method must be called first.")
+    if (this.#begun) return;
+    throw new DatabaseError("The .begin() method must be called first.");
   }
 }
 
@@ -185,19 +183,15 @@ return;
 
 client.on("ready", async () => {
   const cacheManager_test1 = new CacheManager("normal");
-  await cacheManager_test1.begin()
+  await cacheManager_test1.begin();
   //await cacheManager_test1.clear()
   await cacheManager_test1.set("aaa", 12345);
-  cacheManager_test1.get("aaa")
-    .then(console.log);
-  cacheManager_test1.get("aaa", "test")
-    .then(console.log);
-  await cacheManager_test1.put({aaa:54321, bbb:"abc", ccc:"xyz1234"})
-  cacheManager_test1.get("test", "aaa", "test2", "test", "bbb", "ccc")
-    .then(console.log);
+  cacheManager_test1.get("aaa").then(console.log);
+  cacheManager_test1.get("aaa", "test").then(console.log);
+  await cacheManager_test1.put({ aaa: 54321, bbb: "abc", ccc: "xyz1234" });
+  cacheManager_test1.get("test", "aaa", "test2", "test", "bbb", "ccc").then(console.log);
   await cacheManager_test1.delete("aaa", "ccc", "test", "vvv");
-  cacheManager_test1.get("test", "aaa", "test2", "test", "bbb", "ccc")
-    .then(console.log);
+  cacheManager_test1.get("test", "aaa", "test2", "test", "bbb", "ccc").then(console.log);
   /*for(let i = 0; i < 128; i++) {
     console.log(new Date())
     await cacheManager_test1.put({[`test${i}`]:i})

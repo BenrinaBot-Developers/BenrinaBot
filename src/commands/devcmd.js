9b66aa4bf8a91e876543896dae815165ed5d3d36
uni-kakurenbo <kohma1116@ybb.ne.jp>
2021-11-27 13:14:38 +0900

    test

--- src/commands/devcmd.js
+++ src/commands/devcmd.js
@@ -1,17 +1,17 @@
-'use strict';
+"use strict";
 
 const Discord = require("discord.js");
-const {client, slashguild} = require("../util.js")
+const { client, slashguild } = require("../util.js");
 
-const { Database } = require("../system")
+const { Database } = require("../system");
 const functions = require("../functions.js");
 
-module.exports = async function*(arg1){
-	const db = new Database.CacheManager("normal");
-  //await db.begin();
+module.exports = async function* (arg1) {
+  const db = new Database.CacheManager("normal");
+  await db.begin();
   const result = await db.get("devComd_test");
   console.log(result);
-  yield {embeds:[functions.getEmbed(0x00f521, `Read`, `Result: ${result.data.first()}`, true)]};
+  yield { embeds: [functions.getEmbed(0x00f521, `Read`, `Result: ${result.data.first()}`, true)] };
   await db.set("devComd_test", arg1);
-  return {embeds:[functions.getEmbed(0x00f521, `Write`, `Value: ${arg1}`, true)]};
-}
+  return { embeds: [functions.getEmbed(0x00f521, `Write`, `Value: ${arg1}`, true)] };
+};

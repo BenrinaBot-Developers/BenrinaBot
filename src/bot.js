2c27ebcd00aeeea39299f34d7a3d2fdf174a2457
uni-kakurenbo <kohma1116@ybb.ne.jp>
2021-11-27 16:11:26 +0900

    test mode

--- src/bot.js
+++ src/bot.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 const { Logger, ProcessError, SystemError } = require("./system");
 const util = require("./util");
-const client = util.client
+const client = util.client;
 var prefix = process.env.prefix;
 var version = "1.5.0";
 const db = require("./dbutil.js");
@@ -9,9 +9,9 @@ const db = require("./dbutil.js");
 //実行 てかこの辺コード汚すぎる()
 const Bot = new (class {
   Run() {
-    console.log(util.startsAt)
+    console.log(util.startsAt);
     const bot = require("./command.js");
-    client.on("messageCreate", message => {
+    client.on("messageCreate", (message) => {
       if (!message.author.bot) {
         db.db.set("msgcount", db.db.get("msgcount") + 1);
         bot.Run(message);
@@ -22,43 +22,44 @@ const Bot = new (class {
 //ready
 
 try {
-client.on("ready", () => {
-
-  /*process.on('uncaughtException', (err, origin) => {
+  client.on("ready", () => {
+    /*process.on('uncaughtException', (err, origin) => {
     new ProcessError(err, origin).log();
   });*/
 
-  console.log(`${client.user.tag} でログインしています。`);
-  Logger.log(Logger.Type.START_UP, {date: util.startsAt})
-  Bot.Run();
-  require("./test.js")
+    console.log(`${client.user.tag} でログインしています。`);
+    Logger.log(Logger.Type.START_UP, { date: util.startsAt });
+    Bot.Run();
+    require("./test.js");
 
-  return;
-  let count = 0;
-  setInterval(() => {
-    const srvkz = client.guilds.cache
-      .map(guild => guild.memberCount)
-      .reduce((p, c) => p + c);
-    const list = [
-      `helpはb!help/パッチノートは${prefix}patch`,
-	  `1.0.0-dev版のbeta機能を全鯖に配信中!!`,//notice
-	  `Ver.${version}/Node.js v16/Discord.js v13導入！！`,
-      `起動秒数${db.db.get("time")}秒...
+    return;
+    let count = 0;
+    setInterval(() => {
+      const srvkz = client.guilds.cache.map((guild) => guild.memberCount).reduce((p, c) => p + c);
+      const list = [
+        `helpはb!help/パッチノートは${prefix}patch`,
+        `1.0.0-dev版のbeta機能を全鯖に配信中!!`, //notice
+        `Ver.${version}/Node.js v16/Discord.js v13導入！！`,
+        `起動秒数${db.db.get("time")}秒...
 	  /処理したメッセージの数...${db.db.get("msgcount")}個...`,
-      `${client.guilds.cache.size}サーバーを監視/鯖の合計人数${srvkz}`
-    ];
+        `${client.guilds.cache.size}サーバーを監視/鯖の合計人数${srvkz}`,
+      ];
 
-    client.user.setActivity(list[count], { type: "PLAYING" });
-    count++;
-    db.db.set("time", db.db.get("time") + 5);
+      client.user.setActivity(list[count], { type: "PLAYING" });
+      count++;
+      db.db.set("time", db.db.get("time") + 5);
 
-    if (count >= list.length) {
-      count = 0;
-    }
-  }, 1000 * 5);
-});
+      if (count >= list.length) {
+        count = 0;
+      }
+    }, 1000 * 5);
+  });
 
-client.login(process.env.token).catch(console.error);
-} catch(e) {
+  if (process.env.RUNNING_PLACE === "replit") {
+    client.login(process.env.TOKEN).catch(console.error);
+  } else if (process.env.RUNNING_PLACE === "local") {
+    client.login(process.env.TEST_TOKEN).catch(console.error);
+  }
+} catch (e) {
   new SystemError(e).log();
 }

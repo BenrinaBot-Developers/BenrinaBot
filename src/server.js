3fb0f5f1c92c84b24fffa78bb5e73495459a745d
うに <64454054+uni-kakurenbo@users.noreply.github.com>
2021-11-27 01:53:48 +0000

    reboot system

---
+++ src/server.js
@@ -0,0 +1,43 @@
+'use strict';
+const express = require('express');
+
+const app = express();
+
+app.get('/reboot', (req, res) => {
+  res.send("exit");
+  require("../reboot.js");
+});
+
+app.listen('3000', () => {
+    console.log('Application started');
+});
+
+// リクエストの処理
+/*
+function doRequest(req, res) {
+  let url = req.url;
+  if ("/" == url) {
+    fs.readFile(`${__dirname}/web/index.html`, "UTF-8", function (err, data) {
+      res.writeHead(200, {"Content-Type": "text/html"});
+      res.write(data);
+      res.end();
+    });
+  } else if(url.startsWith("/")) {
+    console.log(url);
+    if (url.match(/\.js$/)) {
+      //console.log(url)
+      fs.readFile(`${__dirname}/web${url}`, "UTF-8", function (err, data) {
+        res.writeHead(200, {"Content-Type": "text/javascript"});
+        res.write(data); 
+        res.end();
+      });
+      } else if (url.match(/\.png$/)) {
+        res.writeHead(200, {'Content-Type': `image/png; charset=utf-8`});
+        var image = fs.readFileSync(`${__dirname}/web${url}`, "binary");
+        res.end(image, "binary");
+    }
+  }
+}*/
+
+//BOT
+require("./bot.js");

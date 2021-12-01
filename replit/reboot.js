022499b57ec2864227959c927fc7bc2404b04ede
うに <64454054+uni-kakurenbo@users.noreply.github.com>
2021-12-01 19:04:10 +0900

    improve rebooting system

--- replit/reboot.js
+++ replit/reboot.js
@@ -1,9 +1,14 @@
-const exec = require('child_process').exec;
-exec('sh replit/reboot.sh', (error, stdout, stderr) => {
-    console.log(stdout);
-    console.log(stderr);
-    if (error !== null) {
-      console.log(`exec error: ${error}`);
-    }
+const exec = require("child_process").exec;
+exec("sh replit/reboot.sh", (error, stdout, stderr) => {
+  console.log(stdout);
+  console.log(stderr);
+  if (error !== null) {
+    console.log(`exec error: ${error}`);
   }
-);
+});
+
+for (var key in require.cache) {
+  delete require.cache[key];
+}
+
+require("../src/bot.js");

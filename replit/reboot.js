900f6470884e6049a6e07ee9dc19da5415787691
うに <64454054+uni-kakurenbo@users.noreply.github.com>
2021-12-01 19:17:35 +0900

    bug fixed

--- replit/reboot.js
+++ replit/reboot.js
@@ -1,4 +1,7 @@
 const exec = require("child_process").exec;
+
+const { client } = require("../src/util.js");
+
 exec("sh replit/reboot.sh", (error, stdout, stderr) => {
   console.log(stdout);
   console.log(stderr);
@@ -11,4 +14,7 @@ for (var key in require.cache) {
   delete require.cache[key];
 }
 
-//require("../src/bot.js");
+(async () => {
+  await client.destroy();
+  require("../src/bot.js");
+})();

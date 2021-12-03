0370f960f9476f4dd6554d60b274630ee3771313
うに <64454054+uni-kakurenbo@users.noreply.github.com>
2021-12-03 15:47:17 +0900

    bug fixed

--- replit/reboot.js
+++ replit/reboot.js
@@ -3,6 +3,7 @@ const exec = require("child_process").exec;
 const { client } = require("../src/util.js");
 
 exports.reboot = async () => {
+  console.log("reboot");
   exec("sh git pull origin develop", (error, stdout, stderr) => {
     console.log(stdout);
     console.log(stderr);
